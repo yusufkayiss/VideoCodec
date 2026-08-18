3eb371a02f354e2d38894e7cc789279d85976c71
yusufkayiss <kayissyusuf@gmail.com>
2026-08-18 20:28:48 +0300

    Program.cs açıklamaları eklendi ve gereksiz kaynak dosyaları temizlendi.

--- VideoCodec/Program.cs
+++ VideoCodec/Program.cs
@@ -1,17 +1,23 @@
 namespace VideoCodec
 {
+    // Uygulamanın başlatıcı sınıfı (sadece proje içinde erişilebilir ve bellekte tekil tutulur)
     internal static class Program
     {
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
+        // WinForms ve arayüz bileşenlerinin (dosya seçici diyalogları vb.) çökmeden çalışmasını sağlayan iş parçacığı modeli.
         [STAThread]
         static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+
+            // Ekran ölçekleme (DPI), varsayılan font ve görsel stilleri başlatır.
             ApplicationConfiguration.Initialize();
+
+            // Ana Form1 penceresini ekrana getirir ve uygulamanın mesaj döngüsünü çalıştırır.
             Application.Run(new Form1());
         }
     }
-}
+}s
